f8240754810261720049fca32fda73b7350d5b56
bschellenberger <benjamin.schellenberger@qhrtech.com>
2016-06-24 16:49:26 -0700

    Adding game and Table classes

---
+++ src/main/java/com/bentest/model/Game.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bentest.model;
+
+/**
+ *
+ * @author ben.schellenberger
+ */
+public class Game {
+
+    Player player1;
+
+    Player player2;
+
+    Table table;
+
+    public Game() {
+
+    }
+
+}
